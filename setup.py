a6b2e87b80672f44463480f9ef52978c21a1576d
Satyam shukla <63285317+satyam0051@users.noreply.github.com>
2021-05-20 09:34:49 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(name='probability',
+        version='0.0',
+        description='This is usefull for probability',
+        url='https://github.com/satyam0051/probability',
+        author='Satyam Shukla',
+        author_email='satyam700770@gmail.com',
+        license='Apache License 2.0',
+        packages=['probability'],
+        zip_safe=False,
+
+
+        classifiers=['Development Status ::0-Beta',
+            'Environment::console',
+            'Environment::Web Environment',
+            'intended Audience::End Users/Desktop',
+            'intended Audience::System Administrators',
+            'Operating System::MacOS::MacOS X',
+            'Operating System::Microsoft::Windows',
+            'Operating System::Linux',
+            'Programming Language::Python',
+            'Topic::Communication::Email',
+            'Topic::Software Development::Bug Tracking',],)
